40d7d7fe5e21e98b0c94323bd99ed3964fff812d
Nicu Tofan <nicu.tofan@gmail.com>
2015-03-30 02:59:52 +0300

    Implementation

--- procrun.cc
+++ procrun.cc
@@ -10,6 +10,10 @@
 #include "procrun.h"
 #include "procrun-private.h"
 
+#include <QCoreApplication>
+#include <QProcess>
+
+
 /**
  * @class ProcRun
  *
@@ -18,11 +22,44 @@
 
 /* ------------------------------------------------------------------------- */
 /**
- * Detailed description for constructor.
+ * Constructor connects the slots to Qt object.
  */
-ProcRun::ProcRun()
+ProcRun::ProcRun() :
+    QProcess(),
+    start_time_(QDateTime::currentDateTime ()),
+    end_time_(QDateTime::currentDateTime ()),
+    std_err_output_(),
+    std_out_output_(),
+    all_output_(),
+    b_started_(false),
+    errors_(),
+    states_(),
+    ev_loop_()
 {
     PROCRUN_TRACE_ENTRY;
+
+    connect (this,
+             SIGNAL(error(QProcess::ProcessError)),
+             SLOT(errorSlot(QProcess::ProcessError)));
+    connect (this,
+             SIGNAL(finished(int,QProcess::ExitStatus)),
+             SLOT(finishedSlot(int,QProcess::ExitStatus)));
+    connect (this,
+             SIGNAL(readyReadStandardError()),
+             SLOT(readyReadStandardErrorSlot()));
+    connect (this,
+             SIGNAL(readyReadStandardOutput()),
+             SLOT(readyReadStandardOutputSlot()));
+    connect (this,
+             SIGNAL(started()),
+             SLOT(startedSlot()));
+    connect (this,
+             SIGNAL(stateChanged (QProcess::ProcessState)),
+             SLOT(stateChangedSlot (QProcess::ProcessState)));
+    connect (this,
+             SIGNAL(finished(int,QProcess::ExitStatus)),
+             &ev_loop_,
+             SLOT(quit()));
 
     PROCRUN_TRACE_EXIT;
 }
@@ -30,12 +67,103 @@ ProcRun::ProcRun()
 
 /* ------------------------------------------------------------------------- */
 /**
- * Detailed description for destructor.
+ * Destructor.
  */
 ProcRun::~ProcRun()
 {
     PROCRUN_TRACE_ENTRY;
+    PROCRUN_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void ProcRun::errorSlot (QProcess::ProcessError error)
+{
+    PROCRUN_TRACE_ENTRY;
+    errors_.append (error);
+    PROCRUN_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void ProcRun::finishedSlot (int, QProcess::ExitStatus)
+{
+    PROCRUN_TRACE_ENTRY;
+    // no need to cache them as they are available from QProcess
+    end_time_ = QDateTime::currentDateTime ();
+    PROCRUN_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void ProcRun::readyReadStandardErrorSlot ()
+{
+    PROCRUN_TRACE_ENTRY;
+    QString s (readAllStandardError ());
+    std_err_output_.append (s);
+    all_output_.append (s);
+    PROCRUN_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void ProcRun::readyReadStandardOutputSlot ()
+{
+    PROCRUN_TRACE_ENTRY;
+    QString s (readAllStandardOutput());
+    std_out_output_.append (s);
+    all_output_.append (s);
+    PROCRUN_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void ProcRun::startedSlot ()
+{
+    PROCRUN_TRACE_ENTRY;
+    b_started_ = true;
+    start_time_ = QDateTime::currentDateTime ();
+    PROCRUN_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void ProcRun::stateChangedSlot (QProcess::ProcessState newState)
+{
+    PROCRUN_TRACE_ENTRY;
+    states_.append (newState);
+    PROCRUN_TRACE_EXIT;
+}
+/* ========================================================================= */
+
+/* ------------------------------------------------------------------------- */
+void ProcRun::perform (const QStringList & input)
+{
+    PROCRUN_TRACE_ENTRY;
+
+    // start the program
+    this->start (QIODevice::ReadWrite);
+    if (!this->waitForStarted()) {
+        return;
+    }
+
+    // provide the input
+    foreach (const QString & s, input) {
+        this->write (s.toLatin1 ().constData ());
+    }
+    this->closeWriteChannel();
+
+    // allow it to run
+    // this->waitForFinished();
+    ev_loop_.exec ();
 
     PROCRUN_TRACE_EXIT;
 }
 /* ========================================================================= */
+
+
+
+
+
+
+
--- procrun.h
+++ procrun.h
@@ -12,8 +12,14 @@
 
 #include <procrun/procrun-config.h>
 
-//! brief description
-class PROCRUN_EXPORT ProcRun {
+#include <QList>
+#include <QProcess>
+#include <QDateTime>
+#include <QEventLoop>
+
+//! Helper class to run a process.
+class PROCRUN_EXPORT ProcRun : public QProcess {
+    Q_OBJECT
 
 public:
 
@@ -23,9 +29,59 @@ public:
     //! Destructor.
     virtual ~ProcRun();
 
-protected:
+    qint64
+    runDuration() const {
+        return end_time_.secsTo (start_time_);
+    }
+
+    void perform (
+            const QStringList & input);
+
+    int durationInSeconds () {
+        return end_time_.secsTo (start_time_);
+    }
+
+    int durationInMiliSeconds () {
+        return end_time_.msecsTo (start_time_);
+    }
+
+private slots:
+
+    void
+    errorSlot (
+            QProcess::ProcessError error);
+
+    void
+    finishedSlot (
+            int exitCode,
+            QProcess::ExitStatus exitStatus);
+
+    void
+    readyReadStandardErrorSlot ();
+
+    void
+    readyReadStandardOutputSlot ();
+
+    void
+    startedSlot ();
+
+    void
+    stateChangedSlot (
+            QProcess::ProcessState newState);
+
+public:
+
+    QDateTime start_time_;
+    QDateTime end_time_;
+    QString std_err_output_;
+    QString std_out_output_;
+    QString all_output_;
+    bool b_started_;
+    QList<QProcess::ProcessError> errors_;
+    QList<QProcess::ProcessState> states_;
 
 private:
+    QEventLoop ev_loop_;
 
 };
 
